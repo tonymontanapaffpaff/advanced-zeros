b48148caa0f3a7f0e526ef6af510f6c803a35d46
ANTON LYCH <Luckytoha333@gmail.com>
2019-03-23 11:41:13 +0300

    Check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+let j = 0;
+let i = 2;
+let a = new Array();
+do
+{
+ if (base % i === 0) 
+ {
+  a[j] = i;
+  j++;
+  base = base / i;
+ }
+ else
+ {
+  i++;
+ }
 }
+while (i < base);
+a[j] = i;
+let max = 0;
+for (let i = 0; i < a.length; i++)
+  if (a[i] > max) max = a[i]; 
+return number / max;
+}
+
